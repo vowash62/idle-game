47c714932f4943ea893b04326e6e336a1d933a02
Vyshon <vowash62@harisonburg.k12.va.us>
2019-09-19 14:32:41 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-var score = 1; // Create a variable to store the score in
+var score = 2; // Create a variable to store the score in
 
 var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
+var button = new Button('bucket', btnPress);   // Create a button element
+var hidden = new Button('shot!');
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
 
