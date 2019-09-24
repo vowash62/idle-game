e12468b041c72cc8c2177a9f80d18dcc13107673
Vyshon <vowash62@harisonburg.k12.va.us>
2019-09-24 14:30:47 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,20 +1,20 @@
-var score = 2; // Create a variable to store the score in
+var score = 1; // Create a variable to store the score in
 
-var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('bucket', btnPress);   // Create a button element
-var hidden = new Button('shot!');
-hidden.hide();
-var scr = new Text(score);                    // Create a text element
+var title = new Title('court 101'); // Create a title element
+var button = new Button('score', btnPress);   // Create a button element
+var hidden = new Button('leval!');
+
+var scr = new Text(score); // Create a text element
 
 setInterval(btnPress, 1000);   // Set up a loop
 
 function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
-  if(score > 10) {
+  if(score > 100) {
     hidden.show();
   }
-  if(score > 15) {
+  if(score > 100) {
     hidden.hide(500);
   }
 }
